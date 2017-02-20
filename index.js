0ec085fac4dbca0c044b6d78a6b082269684e9ea
Chiara DeLucia <chiaradelucia@Chiaras-MacBook-Pro.local>
2017-02-20 13:02:28 -0500

    Basic cardand Cloze Card Constructor logic.

--- index.js
+++ index.js
@@ -1,24 +1,61 @@
 var fs = require ('fs');
 
+
 //Basic Card Constructor
 var BasicCard = function (front, back) {
+
+	if (!(this instanceof BasicCard)) {
+        return new BasicCard(front, back);
+    }
+
 	this.front = front;
 	this.back = back;
-}
 
-BasicCard.prototype.append = function() {
-	
 	fs.appendFile('basiccards.js', JSON.stringify(this), function(err, data) {
 			if (err) throw err;
-		console.log('The "data to append" was appended to file!');
+		console.log('The data to append was appended to file!');
+
 	});
-};
+
+}
 
 
-var userCard = new BasicCard(process.argv[2], process.argv[3]);
-
-userCard.append();
+var devCard1 = new BasicCard('What is the function factory called?', 'The constructor function.');
+var devCard2 = new BasicCard('What is a keyword associated with constructor functions?', 'New');
+var devCard3 = new BasicCard('What method is used to add on to a constructor functions object?', 'Prototype');
 
 
 //ClozeCard Constructor
+
+function ClozeCard(text, cloze) {
+
+    if(!(this instanceof ClozeCard)) {
+        return new ClozeCard(text, cloze);
+    };
+
+    this.text = text;
+    this.cloze = cloze;
+
+	fs.appendFile('cloze-card.js', JSON.stringify(this), function(err, data) {
+			if (err) throw err;
+	});
+  
+};
+
+
+
+ClozeCard.prototype.createPartial = function() {
+   return this.partial = (this.text).replace(this.cloze, " ... ");
+};
+
+ClozeCard.prototype.fulltext = function() {
+    return this.text;
+};
+
+var clozeCard1 = new ClozeCard("A constructor is a function that can create reusable objects.", "constructor");
+
+console.log(clozeCard1.createPartial());
+
+
+console.log(clozeCard1.fulltext());
 
